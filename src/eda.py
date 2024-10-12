6cf29180ca2c36f71085fa36d1f2cb0c439aba05
Archana120201-git <archana.b@talentsprint.com>
2024-10-12 08:30:56 +0000

    setting up repository structure

---
+++ src/eda.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt         #  importing some basic libraries to work with the dataset and for visualization
+
+# Load the dataset
+df = pd.read_csv('data/Bank Customer Churn Prediction.csv')
+
+print(df.isnull().sum())                # check for missing values
+
+print(df.describe())                    # generating summary statistics
+
+print(df.mode())                        # check the mode value
+
+# Visualize the distribution of Age
+sns.histplot(df['Age'], kde=True)
+plt.title('Distribution of Age')
+plt.show()
+
+# Visualize the distribution of Balance
+sns.histplot(df['Balance'], kde=True)
+plt.title('Distribution of Balance')
+plt.show()
+
+# Visualize the distribution of Credit Score
+sns.histplot(df['CreditScore'], kde=True)
+plt.title('Distribution of Credit Score')
+plt.show()
+
+# Visualize the distribution of Estimated Salary
+sns.histplot(df['EstimatedSalary'], kde=True)
+plt.title('Distribution of Estimated Salary')
+plt.show()
